2edad58019f2b7597084ef5839b81a4e874d6907
Roberto Romero <roberto@boxtasis.com>
2020-11-04 01:10:02 -0600

    umbra

--- js/base.js
+++ js/base.js
@@ -1,5 +1,11 @@
-window.onload = function(){
-    
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({
+      coverTrigger:false,
+      hover:true
+    });
+    $('.materialboxed').materialbox();
+
     var titles = document.querySelectorAll(".section__title");
     TweenLite.defaultEase = Linear.easeNone;
     $(function () { // wait for document ready
@@ -9,13 +15,13 @@ window.onload = function(){
       var horizontalSlide = new TimelineMax()
       // animate panels
       .to("#js-slideContainer", 1,   {x: "-20%"}, "label1")
-      .from(titles[1], 0.5, {opacity:0}, "label1+=0.5")
+      .from(titles[1], 0.5, {}, "label1+=0.5")
       .to("#js-slideContainer", 1,   {x: "-40%"}, "label2")
-      .from(titles[2], 0.5, {opacity:0}, "label2+=0.5")
+      .from(titles[2], 0.5, {}, "label2+=0.5")
       .to("#js-slideContainer", 1,   {x: "-60%"}, "label3")
-      .from(titles[3], 0.5, {opacity:0}, "label3+=0.5")
+      .from(titles[3], 0.5, {}, "label3+=0.5")
       .to("#js-slideContainer", 1,   {x: "-80%"}, "label4")
-      .from(titles[4], 0.5, {opacity:0}, "label4+=0.5");
+      .from(titles[4], 0.5, {}, "label4+=0.5");
     
     
       // create scene to pin and link animation
@@ -29,8 +35,7 @@ window.onload = function(){
         //.addIndicators() // add indicators (requires plugin)
         .addTo(controller);
       
-      
-      
+    
     });
     //methods
-};
+});
