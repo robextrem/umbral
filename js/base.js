9e20138dfa7145c39838385c043e9f03f615c15e
Roberto Romero <roberto@ulama.co>
2021-09-30 14:20:33 -0500

    cambios

--- js/base.js
+++ js/base.js
@@ -1,46 +1,94 @@
+
 $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.dropdown-trigger').dropdown({
-      coverTrigger:false,
-      hover:true
-    });
-    $('.materialboxed').materialbox();
+ 
+  $('.sidenav').sidenav({alignment:'right'});
+  $('.dropdown-trigger').dropdown({
+    coverTrigger:false,
+    hover:true
+  });
+  //$('.materialboxed').materialbox();
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, {
+    onOpenStart: function(){
+      $("#slide-out").css({zIndex:0});
+    },
+    onCloseStart: function(){
+      $("#slide-out").css({zIndex:999});
 
-    var titles = document.querySelectorAll(".section__title");
-    TweenLite.defaultEase = Linear.easeNone;
-    $(function () { // wait for document ready
-      
-      var controller = new ScrollMagic.Controller();
-    
-      var horizontalSlide = new TimelineMax()
-      // animate panels
-      .to("#js-slideContainer", 1,   {x: "-20%"}, "label1")
-      .from(titles[1], 0.5, {}, "label1+=0.5")
-      .to("#js-slideContainer", 1,   {x: "-40%"}, "label2")
-      .from(titles[2], 0.5, {}, "label2+=0.5")
-      .to("#js-slideContainer", 1,   {x: "-60%"}, "label3")
-      .from(titles[3], 0.5, {}, "label3+=0.5")
-      .to("#js-slideContainer", 1,   {x: "-80%"}, "label4")
-      .from(titles[4], 0.5, {}, "label4+=0.5");
-    
-      $('.grid').masonry({
-        gutter:10,
-        itemSelector: '.grid-item',
-        columnWidth: 200
+    }
+  });
+
+
+
+
+  if($(window).width()>769){
+
+            // Init controller
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      duration: $('section').height(),
+      triggerHook: .025,
+      reverse: true
+    },
+    vertical: false
+  });
+
+
+
+  // Change behaviour of controller
+  // to animate scroll instead of jump
+  controller.scrollTo(function(target) {
+
+    TweenMax.to(window, 0.5, {
+      scrollTo : {
+        x : target,
+        autoKill : true // Allow scroll position to change outside itself
+      },
+      ease : Cubic.easeInOut
+    });
+
+  });
+
+
+  //  Bind scroll to anchor links using Vanilla JavaScript
+  var anchor_nav = document.querySelector('.navigation-link');
+
+  $('.navigation-link').each(function(i,e){
+
+      $(e).on('click', function(evt) {
+          id = $(this).attr('href');
+      if(id !== null && id.length > 0) {
+          evt.preventDefault();
+          var pos= $(id).offset().left;
+
+          controller.scrollTo(pos - 200);
+
+          if(window.history && window.history.pushState) {
+          history.pushState("", document.title, id);
+          }
+      }
       });
+  });
+  window.addEventListener("wheel", onWheel);
 
-      // create scene to pin and link animation
-      new ScrollMagic.Scene({
-        triggerElement: "#js-wrapper",
-        triggerHook: "onLeave",
-        duration: "400%"
-      })
-        .setPin("#js-wrapper")
-        .setTween(horizontalSlide)
-        //.addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
-      
+  function onWheel(event) {
+    //event.preventDefault();
+
+    var normalized;  
+    var delta = event.wheelDelta;
+    var scroll = (window.pageXOffset || document.scrollLeft) - (document.clientLeft || 0) || 0;
     
-    });
-    //methods
+    if (delta) {
+      normalized = (delta % 120) == 0 ? delta / 120 : delta / 12;
+    } else {
+      delta = event.deltaY || event.detail || 0;
+      normalized = -(delta % 3 ? delta * 10 : delta / 3);
+    }
+
+    TweenLite.to(window, 0.4, {scrollTo: {x: scroll + 200 * normalized } });
+  }
+
+
+      }
+
 });
